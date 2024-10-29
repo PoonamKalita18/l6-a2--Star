c50bb5285b51bd1fae7a2e9d734d41ab1f3a7da0
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-29 21:34:25 +0530

    ghj

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.Screen().bgcolor("Cyan")
+board= turtle.Turtle()
+#1st triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+#2nd triangle
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
